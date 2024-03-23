c36931eaa6e745ecdec10d10f636cb5f3b879e64
Rayyan Alam <rayyanalam650@gmail.com>
2024-03-24 02:50:21 +0530

    style: fix height of skeleton

--- client/src/Components/SkeletonLoading/CarouselSkeleton.jsx
+++ client/src/Components/SkeletonLoading/CarouselSkeleton.jsx
@@ -6,8 +6,8 @@ export default function CarouselSkeleton() {
   return (
     <SkeletonTheme highlightColor={"#e3e3e3"} baseColor={"#ececec"}>
       <div className="home-container__top-models__body-item">
-        <div className="body-item--img">
-          <Skeleton width={"100%"} height={"200px"} />
+        <div style={{ aspectRatio: "1/0.75" }} className="body-item--img">
+          <Skeleton width={"100%"} height={"100%"} />
         </div>
         <div className="body-item--content">
           <p>
