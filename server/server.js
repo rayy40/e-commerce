dba9e36aead9c9c1e195cb6be48b0cc820f5a3be
Rayyan Alam <rayyanalam650@gmail.com>
2024-03-24 15:16:38 +0530

    fix cors error

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const stripe = require("./routes/stripe");
 const order = require("./routes/order");
 
 const app = express();
+const port = process.env.PORT || 5000;
+const uri = process.env.DB_URI;
 
 require("dotenv").config();
 
@@ -36,11 +38,8 @@ app.use("/account/login", login);
 app.use("/stripe", stripe);
 app.use("/account/order", order);
 
-const port = process.env.PORT || 5000;
-const uri = process.env.DB_URI;
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
 app.listen(port, console.log(`Server running on port ${port}`));
